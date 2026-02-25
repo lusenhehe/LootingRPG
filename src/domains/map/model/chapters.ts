f19e23a64153b57e22bd07d9819ae6bd8af46fcd
LusenLzn <lusenhehe@163.com>
2026-02-25 22:06:11 +0800

    refactor: 迁移地图章节模型到domains并更新Phase进度

---
+++ src/domains/map/model/chapters.ts
@@ -0,0 +1,59 @@
+import chapterData from '../../../config/map/ChapterData.json';
+import { MapEncounterType, ChapterTheme } from '../../../config/map/mapNode';
+
+interface RawNodeWave {
+  id: string;
+  label?: string;
+  monsters: { monsterId: string }[];
+}
+
+interface RawMapNode {
+  id: string;
+  name: string;
+  recommendedLevel: number;
+  encounterType: string;
+  firstClearRewardGold: number;
+  waves?: RawNodeWave[];
+}
+
+interface RawMapChapter {
+  id: string;
+  name: string;
+  levelRange: string;
+  theme: string;
+  nodes: RawMapNode[];
+}
+
+export interface NodeWave {
+  id: string;
+  label?: string;
+  monsters: { monsterId: string }[];
+}
+
+export interface MapNodeDef {
+  id: string;
+  name: string;
+  recommendedLevel: number;
+  encounterType: MapEncounterType;
+  firstClearRewardGold: number;
+  waves?: NodeWave[];
+}
+
+export interface MapChapterDef {
+  id: string;
+  name: string;
+  levelRange: string;
+  theme: ChapterTheme;
+  nodes: MapNodeDef[];
+}
+
+const rawChapters = chapterData.MAP_CHAPTERS as RawMapChapter[];
+
+export const MAP_CHAPTERS: MapChapterDef[] = rawChapters.map((chapter) => ({
+  ...chapter,
+  theme: chapter.theme as ChapterTheme,
+  nodes: chapter.nodes.map((node) => ({
+    ...node,
+    encounterType: node.encounterType as MapEncounterType,
+  })),
+}));
