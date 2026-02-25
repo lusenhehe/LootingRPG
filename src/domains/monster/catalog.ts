8f92b79f658f9aacfc458cc4b66a33cb81ece709
LusenLzn <lusenhehe@163.com>
2026-02-26 00:33:59 +0800

    0.20.2

--- src/domains/monster/catalog.ts
+++
@@ -1,6 +0,0 @@
-import type { Monster } from '../../types/game';
-import { attachMonsterLore } from './lore';
-import { BOSS_MONSTERS_DATA, NORMAL_MONSTERS_DATA } from './config';
-
-export const NORMAL_MONSTERS: Monster[] = NORMAL_MONSTERS_DATA.map((monster) => attachMonsterLore(monster));
-export const BOSS_MONSTERS: Monster[] = BOSS_MONSTERS_DATA.map((monster) => attachMonsterLore(monster));
